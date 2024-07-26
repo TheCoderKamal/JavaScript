4a4869ff32aa9688322bf75120caba33a91cf16a
TheCoderKamal <kamalsagpariya7@gmail.com>
2024-07-26 15:39:25 +0530

    commited

---
+++ 16] Array Of Object/q1.js
@@ -0,0 +1,46 @@
+let arrOfObjects = [
+    {
+        name: 'John',
+        age: 25
+    },
+    {
+        name: 'Jane',
+        age: 30
+    },
+    {
+        name: 'Tom',
+        age: 20
+    },
+    {
+        name: 'Alice',
+        age: 35
+    },
+    {
+        name: 'Bob',
+        age: 28
+    },
+    {
+        name: 'Sara',
+        age: 32
+    },
+    {
+        name: 'David',
+        age: 22
+    },
+    {
+        name: 'Emily',
+        age: 37
+    },
+    {
+        name: 'Michael',
+        age: 27
+    },
+    {
+        name: 'Sarah',
+        age: 33
+    }
+];
+
+for (var obj of arrOfObjects) {
+    console.log(obj.name + ':'+ obj.age);
+}
