0bda97de155d9077d60f0e200cdbc68baf5e191b
TheCoderKamal <kamalsagpariya7@gmail.com>
2024-09-04 16:59:08 +0530

    commited

--- 31] Basic Todo/script.js
+++ 31] Basic Todo/script.js
@@ -1,24 +1,67 @@
-let input = document.getElementById('inp');
+let task = document.getElementById('inp');
 let add = document.getElementById('add');
 let data = document.getElementById('data');
 let hide = document.getElementById('hide');
 
-add.addEventListener('click', () => {
+// Existing tasks ko display karne ke liye function
+let displayTasks = () => {
+    let existingData = JSON.parse(localStorage.getItem("tasks")) || [];
+    
+    existingData.forEach((taskObj, index) => {
+        data.insertAdjacentHTML('beforeend', `
+            <div id="item" data-index="${index}">
+                <div id="detail">${taskObj.task}</div>
+                <div id="button">
+                    <button class="edit">
+                        <span class="material-symbols-outlined">
+                            edit
+                        </span>
+                    </button>
+                    <button class="delete">
+                        <span class="material-symbols-outlined">
+                            delete
+                        </span>
+                    </button>
+                </div>
+            </div>
+        `);
+    });
 
-    if(input.value == ""){
+    if (existingData.length > 0) {
+        hide.style.display = 'block';
+    }
+};
+
+// Task add karne ke function ko update kiya gaya hai
+let addData = (event) => {
+    event.preventDefault();
+
+    if(task.value == ""){
         alert('Please enter a value');
         return;
     }
+
+    let existingData = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    let obj = {
+        task: task.value,
+    };
+
+    existingData.push(obj);
+
+    localStorage.setItem("tasks", JSON.stringify(existingData));
+
+    // Naye task ko display karna
     data.insertAdjacentHTML('beforeend', `
         <div id="item">
-            <div id="detail">${input.value}</div>
+            <div id="detail">${task.value}</div>
             <div id="button">
-                <button id="edit">
+                <button class="edit">
                     <span class="material-symbols-outlined">
                         edit
                     </span>
                 </button>
-                <button id="delete">
+                <button class="delete">
                     <span class="material-symbols-outlined">
                         delete
                     </span>
@@ -28,6 +71,8 @@ add.addEventListener('click', () => {
     `);
 
     hide.style.display = 'block';
+    task.value = "";  // Clear input field
+}
 
-    input.value = '';
-});
+// Page load par tasks ko display karte hain
+document.addEventListener('DOMContentLoaded', displayTasks);
